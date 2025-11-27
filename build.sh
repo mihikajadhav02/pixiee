107bee8c459ce1e0560a6e4476200cc8d836c180
Mihika <mihika@Mihikas-MacBook-Air.local>
2025-11-27 21:49:35 +0530

    Add PostgreSQL support for Vercel deployment

--- build.sh
+++ build.sh
@@ -3,5 +3,8 @@
 # Install dependencies
 pip install -r requirements.txt
 
+# Run migrations
+python manage.py migrate --noinput
+
 # Collect static files
 python manage.py collectstatic --noinput
--- todo_project/settings.py
+++ todo_project/settings.py
@@ -74,12 +74,24 @@ WSGI_APPLICATION = 'todo_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+import dj_database_url
+
+# Use PostgreSQL on Vercel, SQLite locally
+if os.environ.get('POSTGRES_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get('POSTGRES_URL'),
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
